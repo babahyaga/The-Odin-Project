f6bdc2c05849a8fe274796ca739684a75f8e7fb2
Stephen Lang <stephenlang91@gmail.com>
2019-03-24 19:58:12 -0400

    added tests for game

---
+++ Testing-Rspec/Connect_Four/spec/Game_spec.rb
@@ -0,0 +1,25 @@
+Rspec.describe Game do
+
+    describe '#win_check' do 
+
+        it "runs a check after a piece is placed to see whether or not the piece being placed has resulted in a win"do
+        board = Board.new
+        board.state = [['o','o','o','o'],[],[],[],[],[],[]]
+        game = Game.new
+        expect(game.connect_four?(board).to eq(true)
+
+        end    
+    end  
+    
+    describe '#get_move' do 
+        it 'promts the user for a position' do
+            game = Game.new
+            expect(game.get_move(7)).to eq(7)
+        end
+
+        it "onley accepts values between 1-7" do
+            game = Game.new
+            expect(game.get_move(8)).to eq("Please enter a value between 1-7")
+        end
+    end
+end
